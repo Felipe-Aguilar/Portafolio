7f73ac7e5333621309034ad4786474fd8fc833d3
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2022-12-23 14:47:26 -0600

    Agregando más portafolio con imágenes

--- src/componentes/PortafolioTrabajos.js
+++ src/componentes/PortafolioTrabajos.js
@@ -2,54 +2,170 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Goza from '../assets/Goza.jpg';
+import Yalogics from '../assets/Yalogics.png';
+import Dkassa from '../assets/Dkassa.png';
+import South from '../assets/SouthDental.png';
+
+import html from '../assets/html.svg';
+import css from '../assets/css.svg';
+import php from '../assets/php.svg';
+import sass from '../assets/sass.svg';
 
 const PortafolioTrabajos = () => {
     return ( 
-        <PortafolioTrabajosContenedor className='col-12 col-md-10'>
-            <div className='row contenedor-trabajo'>
-                <div className='col-12 col-md-6'>
-                    <img src={Goza} alt="Desarrollador Web | Felipe Aguilar Portafolio" className='img-fluid'/>
-                </div>
-                <div className='col-12 col-md-6'>
-                    <h2>Goza Mx</h2>
-                    <p>
-                        Ecommerce de Ropa para Dama marca totalmente Mexicana, sitio seguro con pasarela de pagos con Mercado Pago y PayPal, envíos con Skydropx. 
-                    </p>
-                    <p>
-                        Conexión entre Ecommerce y Punto de Venta para lectura y edición de inventarios en tiempo real.
-                    </p>
-                    <a href="https://gozamx.com/" target={"_blank"}>
-                        Visitar Sitio <i className="bi bi-link-45deg"></i>
-                    </a>
-                    
-                    <div className='tecnologias'>
-                        <h3>Tecnologías</h3>
-                        <a href="https://wordpress.com/es/" target={"_blank"}>
-                            <i className="bi bi-wordpress"></i> Wordpress
-                        </a>
-                        <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target={"_blank"}>
-                            <i className="bi bi-filetype-html"></i> HTML
-                        </a>
-                        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target={"_blank"}>
-                            <i className="bi bi-filetype-css"></i> CSS
-                        </a>
-                        {/* <a href="https://getbootstrap.com/" target={"_blank"}>
-                            <i className="bi bi-bootstrap-fill"></i> Bootstrap
-                        </a> */}
-                        <a href="https://www.php.net/manual/es/intro-whatis.php" target={"_blank"}>
-                            <i className="bi bi-filetype-php"></i> PHP
-                        </a>
-                        <a href="https://www.mysql.com/" target={"_blank"}>
-                            <i className="bi bi-database-fill"></i> MySQL
-                        </a>
-                        <a href="https://www.microsoft.com/es-mx/sql-server/sql-server-downloads" className='mt-5' target={"_blank"}>
-                            <i className="bi bi-database-fill"></i> SQL Server
-                        </a>
+        <>
+            <PortafolioTrabajosContenedor className='col-12 col-md-10'>
+                <div className='row contenedor-trabajo'>
+                    <div className='col-12 col-md-6'>
+                        {/* <img src={Goza} alt="Desarrollador Web | Felipe Aguilar Portafolio" className='img-fluid'/> */}
                     </div>
+                    <div className='col-12 col-md-6'>
+                        <h2>Goza Mx</h2>
+                        <p>
+                            Ecommerce de Ropa para Dama marca totalmente Mexicana, sitio seguro con pasarela de pagos con Mercado Pago y PayPal, envíos con Skydropx. 
+                        </p>
+                        <p>
+                            Conexión entre Ecommerce y Punto de Venta para lectura y edición de inventarios en tiempo real.
+                        </p>
+                        <a href="https://gozamx.com/" target={"_blank"}>
+                            Visitar Sitio <i className="bi bi-link-45deg"></i>
+                        </a>
+                        
+                        <div className='tecnologias'>
+                            <h3>Tecnologías</h3>
+                            <a href="https://wordpress.com/es/" target={"_blank"}>
+                                <i className="bi bi-wordpress"></i> Wordpress
+                            </a>
+                            <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target={"_blank"}>
+                                <img src={html} alt="Desarrollador web | Felipe Aguilar" /> HTML
+                            </a>
+                            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target={"_blank"}>
+                                <img src={css} alt="Desarrollador web | Felipe Aguilar" /> CSS
+                            </a>
+                            <a href="https://www.php.net/manual/es/intro-whatis.php" target={"_blank"}>
+                                <img src={php} alt="Desarrollador web | Felipe Aguilar" /> PHP
+                            </a>
+                            <a href="https://www.mysql.com/" target={"_blank"}>
+                                <i className="bi bi-database-fill"></i> MySQL
+                            </a>
+                            <a href="https://www.microsoft.com/es-mx/sql-server/sql-server-downloads" target={"_blank"}>
+                                <i className="bi bi-database-fill"></i> SQL Server
+                            </a>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
-        </PortafolioTrabajosContenedor>
+            </PortafolioTrabajosContenedor>
+
+            <PortafolioTrabajosContenedor className='col-12 col-md-10 mt-5'>
+                <div className='row contenedor-trabajo'>
+                    <div className='col-12 col-md-6'>
+                        <img src={Yalogics} alt="Desarrollador Web | Felipe Aguilar Portafolio" className='img-fluid'/>
+                    </div>
+                    <div className='col-12 col-md-6'>
+                        <h2>Yalogics</h2>
+                        <p>
+                            Empresa dedicada a la creación de Ecommerce en CMS y desarrollos web en general.
+                        </p>
+                        <p>
+                            Desarrollo de sitio web informativo con portafolio y medio de contacto dentro del sitio.
+                        </p>
+                        <a href="https://yalogics.com/" target={"_blank"}>
+                            Visitar Sitio <i className="bi bi-link-45deg"></i>
+                        </a>
+                        
+                        <div className='tecnologias'>
+                            <h3>Tecnologías</h3>
+                            <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target={"_blank"}>
+                                <img src={html} alt="Desarrollador web | Felipe Aguilar" /> HTML
+                            </a>
+                            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target={"_blank"}>
+                                <img src={css} alt="Desarrollador web | Felipe Aguilar" /> CSS
+                            </a>
+                            <a href="https://sass-lang.com/" target={"_blank"}>
+                                <img src={sass} alt="Desarrollador web | Felipe Aguilar" /> SASS
+                            </a>
+                            <a href="https://getbootstrap.com/docs/4.0/getting-started/introduction/" target={"_blank"}>
+                                <i className="bi bi-bootstrap-fill"></i> Bootstrap
+                            </a>
+                            <a href="https://www.php.net/manual/es/intro-whatis.php" target={"_blank"}>
+                                <img src={php} alt="Desarrollador web | Felipe Aguilar" /> PHP
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </PortafolioTrabajosContenedor>
+
+            <PortafolioTrabajosContenedor className='col-12 col-md-10 mt-5'>
+                <div className='row contenedor-trabajo'>
+                    <div className='col-12 col-md-6'>
+                        <img src={Dkassa} alt="Desarrollador Web | Felipe Aguilar Portafolio" className='img-fluid'/>
+                    </div>
+                    <div className='col-12 col-md-6'>
+                        <h2>Dkassa</h2>
+                        <p>
+                            Ecommerce de Venta de Muebles de alta calidad en Nuevo León Monterrey.
+                        </p>
+                        <p>
+                            Pagos seguros dentro del sitio con Paypal y OpenPay. Gestión de envíos a todo México.
+                        </p>
+                        <a href="https://dkassa.com.mx/" target={"_blank"}>
+                            Visitar Sitio <i className="bi bi-link-45deg"></i>
+                        </a>
+                        
+                        <div className='tecnologias'>
+                            <h3>Tecnologías</h3>
+
+                            <a href="https://wordpress.com/es/" target={"_blank"}>
+                                <i className="bi bi-wordpress"></i> Wordpress
+                            </a>
+
+                            <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target={"_blank"}>
+                                <img src={html} alt="Desarrollador web | Felipe Aguilar" /> HTML
+                            </a>
+                            <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target={"_blank"}>
+                                <img src={css} alt="Desarrollador web | Felipe Aguilar" /> CSS
+                            </a>
+                            <a href="https://devdocs.io/javascript/" target={"_blank"}>
+                                JavaScript
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </PortafolioTrabajosContenedor>
+
+            <PortafolioTrabajosContenedor className='col-12 col-md-10 mt-5'>
+                <div className='row contenedor-trabajo'>
+                    <div className='col-12 col-md-6'>
+                        <img src={South} alt="Desarrollador Web | Felipe Aguilar Portafolio" className='img-fluid'/>
+                    </div>
+                    <div className='col-12 col-md-6'>
+                        <h2>South Dental</h2>
+                        <p>
+                            Consultorio Dental
+                        </p>
+                        <p>
+                            Sitio Web Informativo de South Dental Center México.
+                        </p>
+                        <a href="https://southdentalmexico.com/" target={"_blank"}>
+                            Visitar Sitio <i className="bi bi-link-45deg"></i>
+                        </a>
+                        
+                        <div className='tecnologias'>
+                            <h3>Tecnologías</h3>
+
+                            <a href="https://wordpress.com/es/" target={"_blank"}>
+                                <i className="bi bi-wordpress"></i> Wordpress
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </PortafolioTrabajosContenedor>
+        
+        </>
     );
 }
 
@@ -60,7 +176,11 @@ const PortafolioTrabajosContenedor = styled.div`
     border-radius: 10px;
     padding: 50px;
 
-    div{
+    @media screen and (max-width: 575px){
+        padding: 20px;
+    }
+
+    .contenedor-trabajo{
         img{
             border-radius: 10px;
         }
@@ -77,19 +197,18 @@ const PortafolioTrabajosContenedor = styled.div`
             padding: 10px 15px;
             background: #fff;
             color: #2a2a2a;
-            outline: none;
             
             i{
                 color: #2a2a2a;
-                font-size: 18px;
             }
 
             &:hover{
                 color: #2a2a2a; 
             }
         }
+
         .tecnologias{
-            margin-top: 8rem;
+            margin-top: 100px;
 
             h3{
                 font-size: 20px;
@@ -97,6 +216,8 @@ const PortafolioTrabajosContenedor = styled.div`
             a{
                 font-size: 15px;
                 margin-right: 10px;
+                margin-bottom: 5px;
+                display: inline-block;
             }
         }
     }
